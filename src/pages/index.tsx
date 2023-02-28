1749e336ffa319fa38730417af1f3304e0fe1f75
James Brown <james@silverorange.com>
2023-02-28 16:07:43 -0500

    add input form for creating new todos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,21 @@ const Home: NextPage = () => {
     <div>
       <pre>{JSON.stringify(state.value)}</pre>
       <pre>{JSON.stringify(state.context)}</pre>
+      <div>
+        {state.matches("Todos Loaded") && (
+          <button onClick={() => send({ type: "Create new" })}>Create New</button>
+        )}
+        {state.matches("Creating new todo.Showing form input") && (
+          <input
+            onChange={(e) => {
+              send({
+                type: "Form input changed",
+                value: e.target.value,
+              });
+            }}
+          ></input>
+        )}
+      </div>
     </div>
   );
 };
